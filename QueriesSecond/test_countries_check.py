053f9b4811955d181fc0fa4d019594d9c8d0c6bf
viaviare <viaviare@yandex.ru>
2017-12-18 14:28:12 +0300

    add_homework9

--- QueriesSecond/test_countries_check.py
+++ QueriesSecond/test_countries_check.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.select import Select
 
 
 @pytest.fixture
@@ -22,7 +22,7 @@ def test_launch_query(driver):
     inputElement.send_keys("admin")
     ButtonElement = driver.find_element_by_css_selector("button[name=login]")
     ButtonElement.click()
-    WebDriverWait(driver,10).until(EC.title_is("My Store")) #проверить загрузку страницы
+    WebDriverWait(driver,10).until(EC.title_is("My Store"))
     #--------------------------
 
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
@@ -52,4 +52,24 @@ def test_launch_query(driver):
     countries_list_sort.sort()
     assert(countries_list_sort == countries_list)
 
+    #----------------------
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    countries_q = driver.find_elements_by_css_selector("tr.row")
+    for i in range(len(countries_q)):
+        countries_q[i].find_element_by_css_selector("a").click()
+        zones_q = driver.find_elements_by_css_selector("table#table-zones tr")
+        zones_list = list()
 
+        for j in range(1, len(zones_q)):
+            zones_in = zones_q[j].find_elements_by_xpath(".//td[3]/select/option")
+            for t in range(len(zones_in)):
+                if zones_in[t].get_attribute("selected")== "true":
+                    zones_list.append(zones_in[t].text)
+                    break
+
+        zones_list_sort = list(zones_list)
+        zones_list_sort.sort()
+
+        assert(zones_list_sort == zones_list)
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+        countries_q = driver.find_elements_by_css_selector("tr.row")
